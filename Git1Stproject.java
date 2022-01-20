89d5a68197a368b800bd7c4fa5c819e592f3dc09
Rohit shelke <rohits@heaptrace.net>
2022-01-20 19:10:55 +0530

    3 lines print msg

---
+++ Git1Stproject.java
@@ -0,0 +1,15 @@
+public class Git1Stproject
+{
+	public static void main(String[] args){
+
+	System.out.println("hii");
+	System.out.println("hello");
+	System.out.println("How are you");
+	
+
+	}
+
+
+
+
+}
